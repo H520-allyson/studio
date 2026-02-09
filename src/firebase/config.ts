38e97efad8e10d729d1ce9f94c430f338e9e7738
Allyson Banzuela <allysonbanzuela@gmail.com>
2026-02-09 08:32:48 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2566474323-dc85e",
+  "appId": "1:867616133887:web:3d26d0064e8bc9b45c32ca",
+  "apiKey": "AIzaSyCc_stC7tSokN2oFhS6HQ-HsN1uxZm6qdc",
+  "authDomain": "studio-2566474323-dc85e.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "867616133887"
+};
